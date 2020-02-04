9a87aeaec53401cd523b6c591d371f2ae3acf141
Paco <paco.sanchez.ram@gmail.com>
2020-02-04 04:07:29 -0600

    url change

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-exports.DATABASE_URL = process.env.DATABASE_URL || "mongodb+srv://admin:admin@cluster0-pqibx.mongodb.net/delta?retryWrites=true&w=majority";
+exports.DATABASE_URL = process.env.DATABASE_URL || "mongodb://localhost/delta";
 exports.PORT = process.env.PORT || 8080;
